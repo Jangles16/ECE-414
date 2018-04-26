e106386517879097687d7ee0fe232b557ce944d0
Josh Andrews <jangles1602@gmail.com>
2018-04-26 12:59:33 -0400

    Added .m file

---
+++ Final Project/Final_Project.m
@@ -0,0 +1,13 @@
+%% ECE 414 - Final Project
+% Written by Josh Andrews, Riley McKay
+
+%% Step One, Get the TF's in here and other variables needed
+% Need N,D,C equations, and most parameters (make data structure?)
+
+
+%% Step Two, Do Root Locus of Plants
+% Plot change in poles as Kt and Mi change.  Mi=cartridge mass, Kt = motor
+% torque constant.
+
+
+%% Step Three, Start Looking at Controllers
