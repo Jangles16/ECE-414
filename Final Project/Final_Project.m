20916b66b5a4a6a4fd7e4be8dd3683b24a2f61cf
Josh Andrews <jangles1602@gmail.com>
2018-05-09 17:00:40 -0400

    Worked on testing all setups

--- Final Project/Final_Project.m
+++ Final Project/Final_Project.m
@@ -13,19 +13,22 @@ d.Gv = 5;               %Voltage amplifier gain (V/V)
 d.Ks = 25;              %position sensor gain (V/m)
 d.Rp = 0.0075;          %pulley radius (m)
 d.Jb = 1.0125e-6;       %belt inertia
-d.Mc = 0.150;           %nominal cartridge mass (kg)
+d.Mc = [0.050 0.150 0.250];           %nominal cartridge mass (kg)
 d.Jc = d.Mc * (d.Rp)^2; %nominal cartridge inertia
+
+for i = 1:3
+    
+    d.Jx(i) = (d.Jp)*2 + d.Js + d.Jb + d.Jc(i);   %The constant part of inertia (only motor inertia change) when fixing Cartridge mass.
+end
 
 %% Find the Nominal plants
 % Need to do this for all for motors
 
-% Find Total inertia - motor inertia
-d.Jx = (d.Jp)*2 + d.Js + d.Jb + d.Jc;   %The constant part of inertia (only motor inertia change) when fixing Cartridge mass.
-
 for m = 1:4
+    
     motor = MotorNum(m); % Choose one of the four available motors
     % Inertia calculations
-    motor.Jsys = motor.Jm + d.Jx;   % Calculate total system inertia
+    motor.Jsys = motor.Jm + d.Jx(2);   % Calculate total system inertia
     
     motor.Numerator = (d.Gv * motor.Ktnom * d.Ks * d.Rp)/(motor.L * motor.Jsys);
     motor.sOneTerm = (motor.R/motor.L)+(motor.Bm/motor.Jsys);
@@ -44,8 +47,8 @@ end
 
 %% Step Two, Do Root Locus of Plants as Parameters Change
 % Plot change in poles as Kt and Mi change.  Mi=cartridge mass, Kt = motor
-% torque constant.
-d.Mrange = [50,100,150,200,250]./1000;
+% torque constant. Will have 3 variations of each parameter (Km and Mi)
+
 %%% Still Need to do (maybe)  %%%
 
 
@@ -73,76 +76,90 @@ acc = dfun(tfun);
 
 
 %% Controller
+% need to find out which plant setup to optimize for, might actually be several we
+% have to look at. I'm thinking right now that Motor 1 with full ink and
+% high inertia will be the slowest to respond.
+
 C=20;   % P type to test output plots
 
 %%% will need a method to find controller
+%%% Step is actually faster than ours, so need to experiment.  Also want to
+%%% keep phase margin high.
 
 
 
 
 %% Calculate Loop TF
-L=C*Plant(1).G;
+for i=1:4
+    L=C*Plant(i).G;
 
 
-%% Velocity Transfer Function
-% The basic plant (input/output at each end of -kt loop repectivly) is Z
-Znum=(Plant(1).Ktnom)/(Plant(1).L*Plant(1).Jsys);
-Zden =(s^2 + (Plant(1).R/Plant(1).L)*s+(Plant(1).Bm/Plant(1).Jsys)*s+((Plant(1).Ktnom^2 + Plant(1).R*Plant(1).Bm)/(Plant(1).L*Plant(1).Jsys)));
-Z=Znum/Zden;
+    %% Velocity Transfer Function
+    % The basic plant (input/output at each end of -kt loop repectivly) is Z
+    Znum=(Plant(i).Ktnom)/(Plant(i).L*Plant(i).Jsys);
+    Zden =(s^2 + (Plant(i).R/Plant(i).L)*s+(Plant(i).Bm/Plant(i).Jsys)*s+((Plant(i).Ktnom^2 + Plant(i).R*Plant(i).Bm)/(Plant(i).L*Plant(i).Jsys)));
+    Z=Znum/Zden;
 
-Vel_tf = d.Ks*C*Z*d.Gv*d.Rp/(1+L);
-
-
-%% System Transfer Function
-Sys_tf = L/(1+L);      %whole system (position out)
+    Plant(i).Vel_tf = d.Ks*C*Z*d.Gv*d.Rp/(1+L);
 
 
-%% Voltage Transfer Function
-Volt_tf = (C*d.Gv)/(1+L);
+    %% System Transfer Function
+    Plant(i).Sys_tf = L/(1+L);      %whole system (position out)
 
 
-%% Current Transfer Function
-Curr_plant_num = (d.Gv/Plant(1).L)*(s+(Plant(1).Bm/Plant(1).Jsys));
-Curr_plant_den = s^2+((Plant(1).R/Plant(1).L)+(Plant(1).Bm/Plant(1).Jsys))*s + ((((Plant(1).Ktnom)^2)+Plant(1).R*Plant(1).Bm)/(Plant(1).L*Plant(1).Jsys));
-Curr_tmp = Curr_plant_num/Curr_plant_den;
-Curr_tf = (C*Curr_tmp)/(1+L);
+    %% Voltage Transfer Function
+    Plant(i).Volt_tf = (C*d.Gv)/(1+L);
+
+
+    %% Current Transfer Function
+    Curr_plant_num = (d.Gv/Plant(i).L)*(s+(Plant(i).Bm/Plant(i).Jsys));
+    Curr_plant_den = s^2+((Plant(i).R/Plant(i).L)+(Plant(i).Bm/Plant(i).Jsys))*s + ((((Plant(i).Ktnom)^2)+Plant(i).R*Plant(i).Bm)/(Plant(i).L*Plant(i).Jsys));
+    Curr_tmp = Curr_plant_num/Curr_plant_den;
+    Plant(i).Curr_tf = (C*Curr_tmp)/(1+L);
+end
 
 
 %% Position Response v Time
-[Pos_act,tref]=lsim(Sys_tf,pos,tfun);
-figure(1);clf;
-plot(tref,Pos_act)
-hold on
-plot(tfun,pos)      %variables from spluse function
-legend('Actual','Reference')
-title('Whole System')
+% figure(1);clf;
+% for i=1:4
+%     [Pos_act,tref]=lsim(Plant(i).Sys_tf,pos,tfun);
+%     plot(tref,Pos_act)
+%     hold on
+%     
+% end
+% plot(tfun,pos)      %variables from spluse function
+% legend('1','2','3','4','ref')
+% title('Whole System')
 
+% %% Voltage Response v Time
+% [Volt_act,tref]=lsim(Volt_tf,pos,tfun);
+% figure(3);clf;
+% plot(tref,Volt_act)
+% title('Voltage')
+% 
+% 
+% %% Current Response v Time
+% [Curr_act,tref]=lsim(Curr_tf,pos,tfun);
+% figure(4);clf;
+% plot(tref,Curr_act)
+% title('Current')
+% 
+% 
+% %% Velocity Response v Time
 
-%% Voltage Response v Time
-[Volt_act,tref]=lsim(Volt_tf,pos,tfun);
-figure(3);clf;
-plot(tref,Volt_act)
-title('Voltage')
-
-
-%% Current Response v Time
-[Curr_act,tref]=lsim(Curr_tf,pos,tfun);
-figure(4);clf;
-plot(tref,Curr_act)
-title('Current')
-
-
-%% Velocity Response v Time
-[Vel_act,tref]=lsim(Vel_tf,pos,tfun);
-figure(2);clf;
-plot(tref,Vel_act)
-title('better Xdot')
-
-
+% figure(2);clf;
+% plot(tref,Vel_act)
+% title('better Xdot')
+% 
+% 
 %% Velocity Versus Position
 figure(6);clf;
-plot(pos*100,Vel_act*100)
-hold on
+for i=1:4
+    [Vel_act,tref]=lsim(Plant(i).Vel_tf,pos,tfun);
+    plot(pos*100,Vel_act*100)
+    hold on
+end
+legend('1','2','3','4')
 plot([2 2],ylim,'--');      % 2cm edge
 hold on
 plot([24 24],ylim,'--');    % 24cm edge
@@ -161,15 +178,22 @@ title('Velocity vs Position')
 xlabel('Position (cm)')
 ylabel('Velocity (cm/s)')
 xlim([0 27])
-
-%% Power Calculation and Plot
-
-figure(7);clf;
-Power_act = abs(Volt_act.*Curr_act);
-plot(tref, Power_act)
-title('Power')
-xlabel('Time (s)')
-ylabel('Power (W)')
+% 
+% %% Power Calculation and Plot
+% % probably don't have to plot this one, just need the max
+% 
+% figure(7);clf;
+% Power_act = abs(Volt_act.*Curr_act);
+% plot(tref, Power_act)
+% title('Power')
+% xlabel('Time (s)')
+% ylabel('Power (W)')
+% 
+% %% There are a few more plots requested
+% 
+% 
+% 
+% %% 
 
 
 
